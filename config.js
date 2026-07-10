fa40d8c301ccf12dc46180db2fbd72f6108b2b7a
Jade <jadeanandethpaisarn@gmail.com>
2026-07-10 15:39:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 var CONFIG = {
-  GAS_API_URL:       "https://script.google.com/macros/s/AKfycbwuq9F7Eh3Va9A86YYa-1-DxnE8-1BVvANKfDthNP99rPujjmTY0tCU9cVs1-7cYs2f/exec",
+  GAS_API_URL:       "https://script.google.com/macros/s/AKfycbznNuE6rJhrn_gZC7DIwDENfBKVBwSHm4zy-MbvuG5_28BoOkURKU5QFjkLbttOnk99Ag/exec",
   // ── Cloudinary Config ──
   CLOUDINARY_CLOUD:  "dqgszn5b1",
   CLOUDINARY_PRESET: "Bntires",
